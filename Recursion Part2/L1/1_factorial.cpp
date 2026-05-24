0f4dcdaba1a76b2282f8b23dc27eb4f2409d58e1
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-05-24 10:57:07 +0530

    done

---
+++ Recursion Part2/L1/1_factorial.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int factorial(int n){
+
+    if(n == 0){
+        return 1;
+    }
+
+    return n * factorial(n-1);
+
+}
+
+int main()
+{
+
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+
+    int fact = factorial(n);
+    cout<<fact<<endl;
+
+   return 0;
+}
